11c9ae729188c9c82d9ddedbb182fc91448b6cd5
WADE HONG <pp159510@gmail.com>
2019-11-07 14:23:14 +0800

    Initial Commit

---
+++ Cells/Cells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cells
+//
+//  Created by wade on 2019/11/2.
+//  Copyright © 2019 wade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ new class/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  new class
+//
+//  Created by wade on 2019/11/2.
+//  Copyright © 2019 wade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var 
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
